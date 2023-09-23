a2d1e4b46ceb84e21bffe2a84ca5ecfe0db97de9
Vedhin Govind <144817637+Vedhin@users.noreply.github.com>
2023-09-23 11:32:41 +0530

    Add files via upload

---
+++ ashaworker.js
@@ -0,0 +1,5 @@
+function completed() {
+    var elem = document.getElementById('status-com');
+    elem.parentNode.removeChild(elem);
+    document.getElementById('completed-').innerHTML="Completed"
+   }
